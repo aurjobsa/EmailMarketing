8450884390fd9e911f221e855c4876f0da37ddf9
Sameer Srivastava <anilanita07@gmail.com>
2025-07-22 12:32:43 +0530

    add azure

--- Client/src/utility/config.js
+++ Client/src/utility/config.js
@@ -1,4 +1,5 @@
-export const BASEURL = "https://emailmarketing-3qin.onrender.com"
+// export const BASEURL = "https://emailmarketing-3qin.onrender.com"
+export const BASEURL = "https://emailai-h2b5hdhqe8e5fvdm.centralindia-01.azurewebsites.net/"
 // export const BASEURL = "http://localhost:5000"
 
 // export const BASEURL = "http://ec2-3-110-219-99.ap-south-1.compute.amazonaws.com:5000"
